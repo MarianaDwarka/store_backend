b65f0fd308660ca9659a3a2d6dd223b07b03524e
MarianaDwarka <mariana_carmona_dwarka@hotmail.com>
2024-06-07 15:17:24 -0600

    JwtUserDetailsService

--- src/main/java/com/marianadwarka/repo/IUserRepo.java
+++ src/main/java/com/marianadwarka/repo/IUserRepo.java
@@ -4,4 +4,7 @@ import com.marianadwarka.model.User;
 
 public interface IUserRepo extends IGenericRepo<User, Integer> {
 
+    //FROM User u WHERE u.username = :username
+    User findOneByUsername(String username);
+
 }
--- src/main/java/com/marianadwarka/security/JwtTokenUtil.java
+++ src/main/java/com/marianadwarka/security/JwtTokenUtil.java
@@ -16,6 +16,7 @@ import java.util.Map;
 import java.util.function.Function;
 import java.util.stream.Collectors;
 
+//Clase S1
 @Component
 public class JwtTokenUtil implements Serializable {
 
