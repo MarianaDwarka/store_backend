2be902af99204539ff8fa4928d7b7ce10163b9c5
MarianaDwarka <mariana_carmona_dwarka@hotmail.com>
2024-06-06 20:22:06 -0600

    Test1

---
+++ src/test/java/com/marianadwarka/controller/CategoryControllerTest.java
@@ -0,0 +1,88 @@
+package com.marianadwarka.controller;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.marianadwarka.dto.CategoryDTO;
+import com.marianadwarka.model.Category;
+import com.marianadwarka.service.ICategoryService;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import java.util.List;
+
+import static org.hamcrest.Matchers.hasSize;
+import static org.hamcrest.Matchers.is;
+import static org.mockito.ArgumentMatchers.any;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@ActiveProfiles("test")
+@AutoConfigureMockMvc(addFilters = false)
+@WebMvcTest(CategoryController.class)
+public class CategoryControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private ICategoryService service;
+
+    @MockBean(name = "categoryMapper")
+    private ModelMapper modelMapper;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    Category CATEGORY_1 = new Category(1, "TV", "Television", true);
+    Category CATEGORY_2 = new Category(2, "PSP", "Play Station Portable", true);
+    Category CATEGORY_3 = new Category(3, "BOOKS", "Some books", true);
+
+    CategoryDTO CATEGORYDTO_1 = new CategoryDTO(1, "TV", "Television", true);
+    CategoryDTO CATEGORYDTO_2 = new CategoryDTO(2, "PSP", "Play Station", true);
+    CategoryDTO CATEGORYDTO_3 = new CategoryDTO(3, "BOOKS", "Some books", true);
+
+    @Test
+    void readAllTest() throws Exception {
+        List<Category> categories = List.of(CATEGORY_1, CATEGORY_2, CATEGORY_3);
+        Mockito.when(service.readAll()).thenReturn(categories);
+        Mockito.when(modelMapper.map(CATEGORY_1, CategoryDTO.class)).thenReturn(CATEGORYDTO_1);
+        Mockito.when(modelMapper.map(CATEGORY_2, CategoryDTO.class)).thenReturn(CATEGORYDTO_2);
+        Mockito.when(modelMapper.map(CATEGORY_3, CategoryDTO.class)).thenReturn(CATEGORYDTO_3);
+
+        mockMvc.perform(MockMvcRequestBuilders
+                        .get("/categories")
+                        .content(MediaType.APPLICATION_JSON_VALUE)
+                )
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.data", hasSize(3)))
+                .andExpect(jsonPath("$.data[1].nameofCategory", is("PSP")));
+    }
+
+    @Test
+    void readByIdTest() throws Exception {
+        final int ID = 1;
+
+        Mockito.when(service.readById(any())).thenReturn(CATEGORY_1);
+        Mockito.when(modelMapper.map(CATEGORY_1, CategoryDTO.class)).thenReturn(CATEGORYDTO_1);
+
+        mockMvc.perform(MockMvcRequestBuilders
+                        .get("/categories/" + ID)
+                        .content(MediaType.APPLICATION_JSON_VALUE)
+                )
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.data", hasSize(1)))
+                .andExpect(jsonPath("$.data[0].nameofCategory", is("TV")));
+    }
+
+
+}
+
+
